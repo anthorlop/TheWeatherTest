25c6e29fe4bbed495ad1e4fe138ed8de39c91270
Antonio Jesús Hormigo López <anthorlop@gmail.com>
2018-09-26 07:32:54 +0200

    load temperature

---
+++ app/src/main/java/com/anthorlop/theweathertest/interfaces/IDetailView.java
@@ -0,0 +1,11 @@
+package com.anthorlop.theweathertest.interfaces;
+
+/**
+ * Created by antonio.hormigo on 26/9/18.
+ */
+
+public interface IDetailView {
+
+    void loadTemperature(String temp);
+
+}
